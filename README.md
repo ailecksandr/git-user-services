77e1f2562aae29f88fad9c01b07e6f7a180a538b
Alexander Orlov <morozhunce@gmail.com>
2023-01-31 13:30:43 +0100

    Add support for ED25519 SSH keys in script

--- README.md
+++ README.md
@@ -3,72 +3,116 @@
 Small application with own service-based architecture.
 
 ### Main purpose:
+
 Changing git user in `.gitconfig` and SSH keys by round-robin.
 
 ### Ruby version
+
 1.9.3 and greater.
 
 ### Usage:
+
 1. Separate users' configuration lines must be commented in `.gitconfig`:
-```bash
-    [user]
-        # email = sample-user-1@gmail.com
-        # name = Sample User 1
-        email = sample-user-2@gmail.com
-        name = Sample User 2
-        # email = sample-user-3@gmail.com
-        # name = Sample User 3
-```
+
+   ```bash
+   [user]
+     # email = sample-user-1@gmail.com
+     # name = Sample User 1
+     email = sample-user-2@gmail.com
+     name = Sample User 2
+     # email = sample-user-3@gmail.com
+     # name = Sample User 3
+   ```
 
 2. (Optional) `<.ssh>` folder must have next tree structure:
-```
-    <.ssh>
-        - id_rsa
-        - id_rsa.pub
-        <git-users>
-            <sample-user-1@gmail.com>
-                 - id_rsa
-                 - id_rsa.pub
-            <sample-user-2@gmail.com>
-                 - id_rsa
-                 - id_rsa.pub   
-```
+
+   ```
+   <.ssh>
+     - id_rsa
+     - id_rsa.pub
+     <git-users>
+       <sample-user-1@gmail.com>
+         - id_rsa
+         - id_rsa.pub
+       <sample-user-2@gmail.com>
+         - id_rsa
+         - id_rsa.pub   
+   ```
 
 3. Execute by
-```bash
-    ruby actions/change.rb
-```
+
+   ```bash
+   ruby actions/change.rb
+   ```
 
 4. After successful execution you will receive information about current user:
-```bash
-    ## Email was replaced to sample-user-3@gmail.com ##
-    ## Name was replaced to Sample User 3 ##
-    ## SSH keys were replaced ##
-    ## <.gitconfig> was changed successfully! ##
-```
+
+   ```bash
+     ## Email was replaced to sample-user-3@gmail.com ##
+     ## Name was replaced to Sample User 3 ##
+     ## SSH keys were replaced ##
+     ## <.gitconfig> was changed successfully! ##
+   ```
+
+5. Current user will be changed to next in order:
+
+   ```bash
+   [user]
+     # email = sample-user-1@gmail.com
+     # name = Sample User 1
+     # email = sample-user-2@gmail.com
+     # name = Sample User 2
+     email = sample-user-3@gmail.com
+     name = Sample User 3
+   ```
 
 Also you can execute code in silent mode - just pass `silent: true` to `Change::Process` service.
 
-5. Current user will be changed to next in order:
-```bash
-    [user]
-    	# email = sample-user-1@gmail.com
-    	# name = Sample User 1
-    	# email = sample-user-2@gmail.com
-    	# name = Sample User 2
-    	email = sample-user-3@gmail.com
-    	name = Sample User 3
-```
+### Script-usage (preferred):
 
-### Script-usage:
 Simplified version of application is placed in `<script>` folder.
+
 1. Move to `<script>` folder:
-```bash
-    cd script
-```
+
+   ```bash
+   cd script
+   ```
 
 2. Run shell script using
-```bash
-    ./guser.sh
-```
+
+   ```bash
+   ./guser.sh
+   ```
+
 Also you can execute code in silent mode - just pass `true` on shell script execution.
+
+   ```bash
+   ./guser.sh true
+   ```
+
+### How to make script fast accessible everywhere in the system
+
+1. Copy script to closer to home folder
+
+   ```bash
+   mkdir -p ~/.ssh-swap && \
+   cp script/guser.rb ~/.ssh-swap/
+   ```
+
+2. Add alias to your console shell
+
+   ```bash
+   # For OhMyZsh users
+   echo '\nalias guser="ruby ~/.ssh-swap/guser.rb"' >> ~/.zshrc && \
+   source ~/.zshrc
+   
+   # For Bash users
+   echo '\nalias guser="ruby ~/.ssh-swap/guser.rb"' >> ~/.bashrc && \
+   source ~/.bashrc
+   ```
+
+3. Execute the next command to swap git user
+
+   ```bash
+   guser
+   ```
--- script/guser.rb
+++ script/guser.rb
@@ -1,151 +1,169 @@
 require 'fileutils'
 
 class ChangeGitUser
-	def self.call(args = {})
-		new(args).call
-	end
+  def self.call(args = {})
+    new(args).call
+  end
 
-	def initialize(params = {})
-		@silent    = params[:silent]
+  def initialize(params = {})
+    @silent = params[:silent]
 
-		prepare!
-	end
+    prepare!
+  end
 
-	def call
-		read!
-		return unless valid?
+  def call
+    read!
+    return unless valid?
 
-		change!
-		check_ssh!
-		write!
-	end
+    change!
+    check_ssh!
+    write!
+  end
 
-	private
+  private
 
-	STATES         				 = [:initialized, :commented, :replaced]
-	FIELDS         				 = [:name, :email]
-	FILE_NAME      				 = "#{ENV['HOME']}/.gitconfig"
-	SSH_FOLDER_NAME_COLUMN = :email
-	SSH_PATH       				 = "#{ENV['HOME']}/.ssh"
-	GIT_USERS_PATH 				 = 'git-users'
+  STATES                 = [:initialized, :commented, :replaced]
+  FIELDS                 = [:name, :email]
+  FILE_NAME              = "#{ENV['HOME']}/.gitconfig"
+  SSH_FOLDER_NAME_COLUMN = :email
+  SSH_PATH               = "#{ENV['HOME']}/.ssh"
+  GIT_USERS_PATH         = 'git-users'
+  KNOWN_HOSTS_PATH       = 'known_hosts'
 
-	attr_reader :config, :silent, :left_border, :right_border, :field_states, :ssh_replaced
+  attr_reader :config, :silent, :left_border, :right_border, :field_states, :ssh_replaced
 
-	STATES.each do |key|
-		define_method "#{key}!" do |field|
-			@field_states[field] = key
-		end
+  STATES.each do |key|
+    define_method "#{key}!" do |field|
+      @field_states[field] = key
+    end
 
-		define_method "#{key}?" do |field|
-			field_states[field] == key
-		end
+    define_method "#{key}?" do |field|
+      field_states[field] == key
+    end
 
-		define_method "has_#{key}?" do
-			field_states.values.any? { |state| state == key }
-		end
-	end
+    define_method "has_#{key}?" do
+      field_states.values.any? { |state| state == key }
+    end
+  end
 
-	def prepare!
-		@field_states = Hash.new
-		FIELDS.each { |field| initialized!(field) }
-	end
+  def prepare!
+    @field_states = Hash.new
+    FIELDS.each { |field| initialized!(field) }
+  end
 
-	def read!
-		@config = File.read(FILE_NAME).split("\n")
-	end
+  def read!
+    @config = File.read(FILE_NAME).split("\n")
+  end
 
-	def valid?
-		@left_border = config.find_index{ |row| row.include? '[user]' }
-		return echo('## There is not user configurations section! ##') if left_border.nil?
+  def valid?
+    @left_border = config.find_index { |row| row.include? '[user]' }
+    return echo('## There is not user configurations section! ##') if left_border.nil?
 
-		@left_border += 1
-		set_right_border!
+    @left_border += 1
+    set_right_border!
 
-		return echo('## User configurations section is empty! ##') if left_border >= right_border
-		true
-	end
+    return echo('## User configurations section is empty! ##') if left_border >= right_border
+    true
+  end
 
-	def change!
-		begin
-			(left_border..right_border).each { |index| change_row!(index) }
-		end while has_commented?
-	end
+  def change!
+    begin
+      (left_border..right_border).each { |index| change_row!(index) }
+    end while has_commented?
+  end
 
-	def check_ssh!
-		return echo('## SSH keys were replaced ##') if ssh_replaced
+  def check_ssh!
+    return echo('## SSH keys were replaced ##') if ssh_replaced
 
-		echo('## SSH keys were not replaced ##')
-	end
+    echo('## SSH keys were not replaced ##')
+  end
 
-	def write!
-		File.open(FILE_NAME, "w") { |file| file.puts(config) }
-		echo('## Config was changed successfully! ##')
-	rescue
-		echo('## Read-only access! ##')
-	end
+  def write!
+    File.open(FILE_NAME, "w") { |file| file.puts(config) }
+    echo('## Config was changed successfully! ##')
+  rescue
+    echo('## Read-only access! ##')
+  end
 
-	def set_right_border!
-		local_right_border = config[left_border..-1].find_index{ |row| row.match(/\A[\[].*[\]]/) }
-		@right_border      = local_right_border.nil? ? config.size - 1 : local_right_border + left_border
-	end
+  def set_right_border!
+    local_right_border = config[left_border..-1].find_index { |row| row.match(/\A[\[].*[\]]/) }
+    @right_border      = local_right_border.nil? ? config.size - 1 : local_right_border + left_border
+  end
 
-	def change_row!(index)
-		FIELDS.each { |field| replace(index, field) }
-	end
+  def change_row!(index)
+    FIELDS.each { |field| replace(index, field) }
+  end
 
-	def replace(index, field)
-		patterns = patterns(field)
+  def replace(index, field)
+    patterns = patterns(field)
 
-		case
-		when config[index].include?(patterns[:commented]) && commented?(field)
-			config[index].gsub!(patterns[:commented], patterns[:uncommented])
-			value = config[index].split('=').last.lstrip
-			echo("## \"#{value}\" is current user ##") if field == :name
-			replace_ssh!(value)                        if field == SSH_FOLDER_NAME_COLUMN
-			replaced!(field)
-		when config[index].include?(patterns[:uncommented]) && initialized?(field)
-			config[index].gsub!(patterns[:uncommented], patterns[:commented])
-			commented!(field)
-		end
-	end
+    case
+    when config[index].include?(patterns[:commented]) && commented?(field)
+      config[index].gsub!(patterns[:commented], patterns[:uncommented])
+      value = config[index].split('=').last.lstrip
+      echo("## \"#{value}\" is current user ##") if field == :name
+      replace_ssh!(value) if field == SSH_FOLDER_NAME_COLUMN
+      replaced!(field)
+    when config[index].include?(patterns[:uncommented]) && initialized?(field)
+      config[index].gsub!(patterns[:uncommented], patterns[:commented])
+      commented!(field)
+    end
+  end
 
-	def patterns(field)
-		{ 
-			commented: "\t# #{field}", 
-			uncommented: "\t#{field}" 
-		}
-	end
+  def patterns(field)
+    {
+      commented:   "\t# #{field}",
+      uncommented: "\t#{field}"
+    }
+  end
 
-	def replace_ssh!(name)
-		return unless ssh_valid?(name)
+  def replace_ssh!(name)
+    identify_encoding!(name)
 
-		FileUtils.cp_r(ssh_files(name), SSH_PATH, remove_destination: true)
-		@ssh_replaced = true
-	end
+    return unless @encoding && File.exist?(public_key(name, encoding: @encoding))
 
-	def ssh_valid?(name)
-		Dir.exist?(user_ssh_path(name)) && ssh_files(name).all? { |file| File.exist?(file) }
-	end
+    remove_current_files!
+    FileUtils.cp_r(ssh_files(name), SSH_PATH, remove_destination: true)
+    @ssh_replaced = true
+  end
 
-	def echo(text)
-		puts(text) unless silent	
-	end
+  def identify_encoding!(name)
+    return unless Dir.exist?(user_ssh_path(name))
 
-	def user_ssh_path(name)
-		File.join(SSH_PATH, GIT_USERS_PATH, name)
-	end
+    @encoding = %i[rsa ed].find { |code| File.exist?(private_key(name, encoding: code)) }
+  end
 
-	def ssh_files(name)
-		[private_key(name), public_key(name)]
-	end
+  def remove_current_files!
+    (Dir.children(SSH_PATH) - [GIT_USERS_PATH, KNOWN_HOSTS_PATH]).each do |file_name|
+      FileUtils.rm(File.join(SSH_PATH, file_name))
+    end
+  end
 
-	def private_key(name)
-		File.join(user_ssh_path(name), 'id_rsa')
-	end
+  def echo(text)
+    puts(text) unless silent
+  end
 
-	def public_key(name)
-		File.join(user_ssh_path(name), 'id_rsa.pub')
-	end
+  def user_ssh_path(name)
+    File.join(SSH_PATH, GIT_USERS_PATH, name)
+  end
+
+  def ssh_files(name)
+    [private_key(name, encoding: @encoding), public_key(name, encoding: @encoding)]
+  end
+
+  def private_key(name, encoding: :rsa)
+    case encoding
+    when :rsa then File.join(user_ssh_path(name), 'id_rsa')
+    when :ed then File.join(user_ssh_path(name), 'id_ed25519')
+    end
+  end
+
+  def public_key(name, encoding: :rsa)
+    case encoding
+    when :rsa then File.join(user_ssh_path(name), 'id_rsa.pub')
+    when :ed then File.join(user_ssh_path(name), 'id_ed25519.pub')
+    end
+  end
 end
 
 ChangeGitUser.(silent: ARGV[0])
